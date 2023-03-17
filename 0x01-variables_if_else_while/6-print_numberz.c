0d07d41adab5238bac4e6afd11cdf7b294f3019b
Mouad bouisselman <mouadbouisse@gmail.com>
2023-03-17 15:57:40 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,11 @@
  **/
 int main(void)
 {
-	char num;
+	int num;
 
-	for (num = '0' ; num <= '9' ; num++)
+	for (num = 0 ; num <= 9 ; num++)
 	{
-		putchar(num);
+		putchar(num + '0');
 	}
 	putchar('\n');
 	return (0);
