7c8a63842a617c2d824d7ac01f9d3b4cb6134f04
Mouad bouisselman <mouadbouisse@gmail.com>
2023-04-17 18:03:49 -0700

    local headerfile

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * struct dog - dog's info
+  * @name: First member
+  * @age: Second
+  * @owner: Third
+  *
+  * Description: dog's informations
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+/**
+  * dog_t - typedef for struct dog
+  */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
